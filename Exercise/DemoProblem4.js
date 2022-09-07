fbed5c13792f062844eab4b2f48725f339c471d7
Gunn-Treepaech <treepaech@kkumail.com>
2022-09-07 15:36:55 +0700

    addLab6

---
+++ Exercise/DemoProblem4.js
@@ -0,0 +1,13 @@
+var Song = function(title,artist){
+    this.title = title;
+    this.artist = artist; 
+    this.play = function()
+    {
+        return console.log('Playing: ' + this.title + ' by ' + this.artist);
+    }
+}
+
+var song1 = new Song('Money', 'Lisa');
+var song2 = new Song('ชีวิตยังคงสวยงาม', 'Bodyslam');
+song1.play();
+song2.play();
